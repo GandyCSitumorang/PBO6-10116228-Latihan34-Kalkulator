cc26c12a0e615ee25c47793325590dfc4a718ddc
MY PC <MY PC@HOME-UEFI1TB.local>
2018-10-09 23:51:21 +0700

    Project Gandy Christian Situmorang 10116228 PBO6

---
+++ src/pbo6/pkg10116228/latihan34/kalkulator/kalkulator.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6.pkg10116228.latihan34.kalkulator;
+
+/**
+ *
+ * @author Gandy Christian Situmorang
+ */
+public class kalkulator {
+    public double value1;
+    public double value2;
+    
+    public double tambahBilangan(){
+        
+        return value1 + value2;
+    }
+    
+    public double kurangBilangan(){
+        return value1 - value2;
+    }
+    
+    public double kaliBilangan(){
+        return value1 * value2;
+    }
+    
+    public double bagiBilangan(){
+        return value1 / value2;
+    }
+    
+    public double sisaBilangan(){
+        return value1 % value2;
+    }
+}
